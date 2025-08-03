acd2123f23147ef82fc5c7d326d2170f0261be61
Mike Dean <miketraindoc@gmail.com>
2025-08-03 16:48:17 -0600

    Undoing the web analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Clinical Trial Accelerator',
@@ -15,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}
-        <Analytics />
       </body>
     </html>
   )
